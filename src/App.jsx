a7519b7020c93256b9b266bd8da2deb37afec90f
Anshul Sharma <AnshulSharma9366@gmail.com>
2026-05-09 22:18:34 -0400

    feat: implement hybrid 3D portfolio with interactive starfield background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Footer from "./pages/Footer";
+import Background3D from "./components/Background3D";
 import "./css/Body.css";
 
 function App() {
   return (
     <>
+      <Background3D />
       <NavTabs />
       <main>
         <Outlet />
